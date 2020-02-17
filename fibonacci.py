6778f49b5b1326c5847b5e7216844aee5515a9c0
angela <joventino.manginsay@gmail.com>
2020-02-17 22:52:06 +0800

    corrected the run time execution

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,5 @@
 #recursion vs. iteration
-#https://github.com/turbocornball/Activity-2-Fibonacci.git 
+#https://github.com/turbocornball/Activity-2-Fibonacci.git
 
 #timing two different ways of solving fibonacci sequence in python
 #results are shown in a graph
@@ -11,65 +11,72 @@ import time                          #for timing each function
 
 
 #lists for storing the execution time on every input
-recur_time = []     
+recur_time = []
 iter_time = []
 
 #list for storing the range of input sizes
-x = []       
+x = []
 
 n = 5
 
+def recur_fibo(n):
+    if n<0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n==1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n==2:
+        return 1
+    else:
+        return recur_fibo(n-1)+recur_fibo(n-2)
+
+def iter_fibo(n):
+    #past = time.time()
+    a = 0
+    b = 1
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2,n):
+            c = a + b
+            a = b
+            b = c
+        return b
+
 #loop for generating input sizes starting from size 5
 while n < 30:
-    n+=5        
+    n+=5
     x.append(n)
     past=time.time()
 
     #recursion function
-    def recur_fibo(n): 
-        if n<0: 
-            print("Incorrect input") 
-        # First Fibonacci number is 0 
-        elif n==1: 
-            return 0
-        # Second Fibonacci number is 1 
-        elif n==2: 
-            return 1
-        else: 
-            return recur_fibo(n-1)+recur_fibo(n-2)
-    
+
+
     (recur_fibo(n))
     recur_time.append(time.time()-past)
 
     #iteration function
-    def iter_fibo(n): 
-        past = time.time()
-        a = 0
-        b = 1
-        if n < 0: 
-            print("Incorrect input") 
-        elif n == 0: 
-            return a 
-        elif n == 1: 
-            return b 
-        else: 
-            for i in range(2,n): 
-                c = a + b 
-                a = b 
-                b = c 
-            return b 
-
-    (iter_fibo(n)) 
+    past=time.time()
+    (iter_fibo(n))
     iter_time.append(time.time()-past)
 
 
-  
-# Function to plot 
-plt.plot(x,recur_time) 
-plt.plot(x,iter_time)
+# Function to plot
+plt.bar(x, recur_time)
+plt.bar(x, iter_time)
 
 plt.title('Fibonacci\n' + '(Execution Time)')
 plt.xlabel('Input Size')
 plt.ylabel('Execution Time')
-# function to show the plot 
-plt.show() 
+# function to show the plot
+plt.show()
+
+
+
+print(recur_time)
+print(iter_time)
